20e434b2b37d2c764973cc0cc01b5367edfa1d3b
Tiago Simões <10504105+tiagorodriguessimoes@users.noreply.github.com>
2022-02-06 21:59:27 +0000

    Feature/day1 part2 (#6)
    * refactor part 1 and complete part 2

--- day1/src/main/scala/com/github/tiagorodriguessimoes/day1sonarsweep/Main.scala
+++ day1/src/main/scala/com/github/tiagorodriguessimoes/day1sonarsweep/Main.scala
@@ -4,13 +4,33 @@ package day1sonarsweep
 import scala.io.Source
 
 @main def Main(args: String*): Unit =
-  val file =
-    scala.io.Source.fromFile(s"src/main/scala/resources/input.txt").getLines()
-  val calculateDiffBetween = file.toSeq
-    .sliding(2)
+
+  val tryStr =
+    scala.io.Source
+      .fromFile(s"src/main/scala/resources/input.txt")
+      .getLines()
+      .map(_.toInt)
+      .toSeq
+
+  println(part1(tryStr))
+  println(part2(tryStr))
+
+def calculateDiffBetweenTandNext(l: Seq[Int]): List[Int] =
+  l.sliding(2)
     .map { case Seq(x, y, _*) =>
-      y.toInt - x.toInt
+      y - x
     }
     .toList
-  val result = calculateDiffBetween.count(_ > 0)
-  print(result)
+
+def part1(sq: Seq[Int]): Int =
+  calculateDiffBetweenTandNext(sq).count(_ > 0)
+
+def part2(sq: Seq[Int]): Int =
+  val grouping3: Seq[Int] = sq
+    .sliding(3)
+    .map { case Seq(x, y, z, _*) =>
+      z + y + x
+    }
+    .toSeq
+
+  calculateDiffBetweenTandNext(grouping3).count(_ > 0)
