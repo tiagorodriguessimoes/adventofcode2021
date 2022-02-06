85990ac45564920f5756de6b80282b581258b943
tiagorodriguessimoes <10504105+tiagorodriguessimoes@users.noreply.github.com>
2022-02-06 21:18:44 +0000

    refactor part 1

--- day1/src/main/scala/com/github/tiagorodriguessimoes/day1sonarsweep/Main.scala
+++ day1/src/main/scala/com/github/tiagorodriguessimoes/day1sonarsweep/Main.scala
@@ -2,15 +2,28 @@ package com.github.tiagorodriguessimoes
 package day1sonarsweep
 
 import scala.io.Source
+import scala.util.{Failure, Success, Try, Using}
+import scala.io.Source
 
 @main def Main(args: String*): Unit =
-  val file =
-    scala.io.Source.fromFile(s"src/main/scala/resources/input.txt").getLines()
-  val calculateDiffBetween = file.toSeq
-    .sliding(2)
-    .map { case Seq(x, y, _*) =>
-      y.toInt - x.toInt
+
+  def part1(tryStr: Option[String]): Option[Int] =
+    val calculateDiffBetween = tryStr.map(a =>
+      a.toSeq
+        .sliding(2)
+        .map { case Seq(x, y, _*) =>
+          y.toInt - x.toInt
+        }
+        .toList
+    )
+    calculateDiffBetween.map(a => a.count(_ > 0))
+
+  val tryStr: Try[String] =
+    Using(Source.fromFile(s"src/main/scala/resources/input.txt"))(_.mkString)
+
+  tryStr match
+    case Failure(exception) => println(s"Error: ${exception.getMessage}")
+    case Success(value) => {
+      val part1Result = part1(Some(value))
+      println(s"Part 1: ${part1Result.getOrElse("No result")}")
     }
-    .toList
-  val result = calculateDiffBetween.count(_ > 0)
-  print(result)
